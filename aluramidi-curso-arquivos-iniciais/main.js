e2f9fec63303f1b878028a1080c12d3094cab95c
melissalopesbueno <106812898+melissalopesbueno@users.noreply.github.com>
2023-11-09 12:13:02 -0300

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,8 +1,22 @@
 
- function tocaSonPom () {
-    document.querySelector('#som_tecla_pom').play();
+ function tocaSon(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+  
  }
 
 const listaDeTeclas = document.querySelectorAll ('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto
+while (contador = listaDeTeclas.length) {
+
+listaDeTeclas[contador].onclick = function () {
+   tocaSom('som_tecla_pom');
+}
+
+  contador = contador + 1;
+
+  console.log(contador);
+
+}
